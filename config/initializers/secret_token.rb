5045806f1f2f8e0193bfc5eefd1c63f4b791112f
Lance Ennen <byennen@gmail.com>
2011-07-28 14:10:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfb::Application.config.secret_token = '57941c6d6108f266491159b05395307761649ab5d4fb76b431b10fd97a88f1d48a67547e0de69c4188a974802ebce281d2dc955198651723da0be9d4a925571c'
