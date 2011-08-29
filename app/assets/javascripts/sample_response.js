3d7a26cb6905bafb4a68df7fc6fcf38c4e957da1
Eryan Cobham <ecobham@searshc.com>
2011-08-29 14:50:48 -0500

    Connected objects and constructor functions

--- app/assets/javascripts/sample_response.js
+++ app/assets/javascripts/sample_response.js
@@ -1,53 +1,81 @@
-$(document).ready(function()
-{
-  $.ajax({
-    type: "GET",
-    url: "sample_response.xml",
-    dataType: "xml",
-    success: parseXml
-  });
-});
-var SearsApiFuncs = function () {
-	var GetProdDet = function ( partNo ) {
-		
-		return 
+// Namespacing everything
+var SearsRegistry = SearsRegistry || {};
+
+SearsRegistry.fbook = function () {
+	// Objects in use
+	var SearsApiFuncs = (function () {
+		var getProdDet = function ( partNo ) {
+				var prodDetObj;
+			
+				return prodDetObj;
+			},
+			goToCheckout = function ( userCart ) {
+			
+			
+			}
+		return {
+			getProdDet : getProdDet,
+			goToCheckout : goToCheckout
 		}
-	return {
-		GetProdDet : GetProdDet
-	}
-};
-
-var RegistryItem = function ( itemXml )
-{
-	var prodDet = null;
-		qtyReq = pareseInt ( $(itemXML).find('QuantityRequested').text() ),
-		qtyBought = parseInt ( $(itemXML).find('QuantityBought').text() ),
-		partNo = $(itemXML).find('PartNumber').text(),
-		catentryId = $(itemXML).find('CatentryID').text();
+	})();
 	
-	this.image = $(itemXML).find('ImageURL').text().split('?')[0];
-	this.itemName = $(itemXML).find('ItemName').text();
-	this.qtyNeeded = (qtyBought < qtyReq) ? (qtyReq - qtyBought) : 0;
-	this.price = $(itemXML).find('ItemPrice').text();
-	this.starrating = null;
+	var UserCart = (function ( ) {
+		var theCart = [],
+		
+			addItemToCart = function ( productToAdd ) {
+				theCart[] = productToAdd;
+			}
+		
+		return {
+			addItemToCart : addItemToCart
+		}
+	})();
 	
-	return {
-		image: this.image,
-		itemName: this.itemName,
-		qtyNeeded: this.qtyNeeded,
-		price: this.price,
-		starrating: this.starrating,
+	var Registry = [];
+	
+	// Constructor Function
+	var RegistryItem = function ( itemXmlObj ) {
+		var prodDet = null;
+			qtyReq = parseInt ( itemXmlObj.find('QuantityRequested').text() ),
+			qtyBought = parseInt ( itemXmlObj.find('QuantityBought').text() ),
+			partNo = itemXmlObj.find('PartNumber').text(),
+			catentryId = itemXmlObj.find('CatentryID').text();
+		
+		this.image = itemXmlObj.find('ImageURL').text().split('?')[0];
+		this.itemName = itemXmlObj.find('ItemName').text();
+		this.qtyNeeded = (qtyBought < qtyReq) ? (qtyReq - qtyBought) : 0;
+		this.price = itemXmlObj.find('ItemPrice').text();
+		this.starrating = null;
+		
+		return {
+			image: this.image,
+			itemName: this.itemName,
+			qtyNeeded: this.qtyNeeded,
+			price: this.price,
+			starrating: this.starrating
+		}
 	}
-}
-RegistryItem.prototype.getProdDet() = function () {
-	var self = this;
-}
+	
+	// Once xml is loaded
+	function parseXml(xml) {
+		var registryLen = 0;
+		$('#registryName').text( $(this).find('EventName').text() );
+		$('#eventDate').text( 'Event Date: ' + $(this).find('EventDate').text() );
+		$(xml).find('GiftRegistryItem').each(function() {
+			Registry[registryLen] = new RegistryItem( $(this) );
+			$('#registryList').append('<li class="registryItem"><img src="' + $(this).find('ImageURL').text() + '"/><p>' + $(this).find('ItemName').text() + '</p></li>');
+		   registryLen++;
+		});
+	}
+	
+	// Load XML registry info once page loads and call parsing function
+	$(document).ready(function() {
+	  $.ajax({
+	    type: "GET",
+	    url: "sample_response.xml",
+	    dataType: "xml",
+	    success: parseXml
+	  });
+	});
 
-function parseXml(xml)
-{
-  $(xml).find("GiftRegistryItem").each(function()
-  {
-    
-    $("#registryList").append('<li class="registryItem"><img src="' + $(this).find("ImageURL").text() + '"/><p>' + $(this).find("ItemName").text() + '</p></li>');
-  });
-}
+}();
